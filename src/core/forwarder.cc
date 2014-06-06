29015548f453948e4439992c75954fe78d779baa
txgbuaa <txgbuaa@gmail.com>
2014-06-06 09:08:57 +0800

    update forwarder.cc

--- src/core/forwarder.cc
+++ src/core/forwarder.cc
@@ -50,16 +50,84 @@ static void make_sockfd_non_block(int sockfd)
 {
 	int flags = fcntl(sockfd, F_GETFL, 0);
 	if(flags < 0)
-        {   
-                lg.err("fcntl error(%s)", strerror(errno));
-                exit(EXIT_FAILURE);
-        }   
+	{   
+		lg.err("fcntl error(%s)", strerror(errno));
+		exit(EXIT_FAILURE);
+	}   
 
 	if(fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) < 0)
 	{
-                lg.err("fcntl error(%s)", strerror(errno));
+		lg.err("fcntl error(%s)", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
+}
+
+static ssize_t recvn_non_block(int sockfd, void* buffer, size_t n)
+{
+	size_t nleft = n;
+	ssize_t nrecv = 0;
+	char* ptr = buffer;
+	int first_recv = 0;
+	while(nleft > 0 )
+	{
+		if(first_recv != 0)
+		{
+			fd_set rset;
+			FD_ZERO(&rset);
+			FD_SET(sockfd, &rset);
+			if(select(sockfd+1, &rset, NULL, NULL, NULL) < 0)
+				return -1;
+		}
+
+		nrecv = recv(sockfd, ptr, nleft, 0);
+		if(first_recv == 0)
+			first_recv = 1;
+		if(nrecv < 0)
+		{
+			if(errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
+				nrecv = 0;
+			else
+				return -1;
+		}
+		else if(nrecv == 0)
+			break; // EOF
+		nleft -= nrecv;
+		ptr += nrecv;
+	}
+	return (n-nleft);
+}
+
+static ssize_t sendn_non_block(int sockfd, void* buffer, size_t n)
+{
+	size_t nleft = n;
+	ssize_t nsend = 0;
+	const char* ptr = buffer;
+	int first_send = 0;
+
+	while(nleft > 0)
+	{
+		if(first_send != 0)
+		{
+			fd_set wset;
+			FD_ZERO(&wset);
+			FD_SET(sockfd, &wset);
+			if(select(sockfd+1, NULL, &wset, NULL, NULL) < 0)
+				return -1
+		}
+		nsend = send(sockfd, ptr, nleft, MSG_NOSIGNAL);
+		if(first_send == 0)
+			first_send = 1;
+		if(nsend <= 0)
+		{
+			if(nsend < 0 && (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))
+				nsend = 0;
+			else
+				return -1;
+		}
+		nleft -= nsend;
+		ptr += nsend;
+	}
+	return (n - nleft);
 }
 
 Forwarder::Forwarder()
